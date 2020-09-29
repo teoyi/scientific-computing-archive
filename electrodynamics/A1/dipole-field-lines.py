69711916652b6dc4681a875ef5687b7bd7b426a8
teoyi <yipengteo@yahoo.com.sg>
2020-09-28 21:58:14 -0700

    Added File description

---
+++ electrodynamics/A1/dipole-field-lines.py
@@ -0,0 +1,13 @@
+'''
+File created for Assignment 1: (Other Problems) for 20 points 
+
+Objective: 
+Figure out how to plot a vector field with preferred software tool
+
+Requirements:
+1. Electric field of dipole in 2D and 3D
+2. Include associated field lines of those fields
+3. Plot a field with non-zero divergence 
+4. Plot a field with non-zero curl 
+5. Make the features obvious and explain how they are obvious 
+'''
